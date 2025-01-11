ee1f790467be60d215b18d69cb2c046df5afde7d
Imtiyaz272 <syedaimtiyaz19@gmail.com>
2025-01-11 10:44:33 +0530

    mongodb connection

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {PORT, mongoURL} from './config.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(mongoURL).then(()=>{
+    console.log("Database is connected");
+})
+.catch((error) => {
+    console.log(error);
+})
+
+app.get('/', (req, res)=>{
+    return res.status(200).send("Welcome to the backend");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`App is listening to port : ${PORT}`);
+});
